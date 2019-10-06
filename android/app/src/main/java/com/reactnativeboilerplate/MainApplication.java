6a226969ee2e03df87b2de233dc68d9ced49d532
Ramandika <theone_rama@yahoo.com>
2019-10-07 06:45:32 +0700

    add one signal for push notif

--- android/app/src/main/java/com/reactnativeboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativeboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativeboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new AsyncStoragePackage(),
             new RNCameraPackage(),
             new VectorIconsPackage(),
